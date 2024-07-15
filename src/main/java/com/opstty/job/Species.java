7655159ca5856af0bbaaa2ac0a93cbc8e4aee89a
Nada NADIRE <nadita8225@gmail.com>
2024-07-15 17:01:38 +0200

    modified:   src/main/java/com/opstty/job/Species.java

--- src/main/java/com/opstty/job/Species.java
+++ src/main/java/com/opstty/job/Species.java
@@ -1,7 +1,5 @@
 package com.opstty.job;
 
-import com.opstty.mapper.SpeciesCountMapper;
-import com.opstty.reducer.SpeciesCountReducer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,28 +8,30 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.log4j.BasicConfigurator;
+
+import com.opstty.mapper.SpeciesCountMapper;
+import com.opstty.reducer.SpeciesCountReducer;
 
 public class Species {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-
         if (otherArgs.length < 2) {
-            System.err.println("Usage: species <in> <out>");
+            System.err.println("Usage: treeSpeciesCount <in> [<in>...] <out>");
             System.exit(2);
         }
-
-        BasicConfigurator.configure();
-        Job job = Job.getInstance(conf, "Species"); 
+        Job job = Job.getInstance(conf, "treeSpeciesCount");
         job.setJarByClass(Species.class);
         job.setMapperClass(SpeciesCountMapper.class);
+        job.setCombinerClass(SpeciesCountReducer.class);
         job.setReducerClass(SpeciesCountReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job,
+                new Path(otherArgs[otherArgs.length - 1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
